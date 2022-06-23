47510728dd1f7c804b2c57e4bfcbc5af57ffa958
Felipe Alves <profelipealves@outlook.com>
2022-06-22 22:54:12 -0300

    atualizando variavek items

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items') || []);
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
     e.preventDefault();
     const text = (this.querySelector('[name=item]')).value;
     const item = {
         text,
-        done: false;
+        done: false
     };
 
     items.push(item);
